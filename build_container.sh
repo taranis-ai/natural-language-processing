f001bc2ff4c16a1dce8169149b51f030cb21888c
llinauer <l.linauer@gmx.at>
2025-03-25 11:13:20 +0100

    Rename INCLUDED_MODEL -> MODEL in build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -6,14 +6,14 @@ cd $(git rev-parse --show-toplevel)
 
 GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
-INCLUDED_MODEL=${INCLUDED_MODEL:-"flair"}
+MODEL=${MODEL:-"flair"}
 
 echo "Building containers for branch ${CURRENT_BRANCH} on ${GITHUB_REPOSITORY_OWNER}"
 
 docker buildx build --file Containerfile \
   --build-arg GITHUB_REPOSITORY_OWNER="${GITHUB_REPOSITORY_OWNER}" \
-  --build-arg MODEL="${INCLUDED_MODEL}" \
+  --build-arg MODEL="${MODEL}" \
   --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:latest" \
   --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:${CURRENT_BRANCH}" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:${INCLUDED_MODEL}" \
+  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:${MODEL}" \
   --load .
