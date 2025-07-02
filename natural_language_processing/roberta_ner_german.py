919cb81afcf481eb5c8d5f749ac61e2ce12dbc90
llinauer <l.linauer@gmx.at>
2025-07-02 09:37:28 +0200

    Add extended output to Roberta models

--- natural_language_processing/roberta_ner_german.py
+++ natural_language_processing/roberta_ner_german.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
-from natural_language_processing.config import Config
+from natural_language_processing.config import Config, ExtendedNerOutput
 from natural_language_processing.predictor import Predictor
+from natural_language_processing.misc import get_word_positions
 
 
 class RobertaGermanNER(Predictor):
@@ -9,12 +10,27 @@ class RobertaGermanNER(Predictor):
     def __init__(self):
         self.model = pipeline(task="ner", model=self.model_name, aggregation_strategy="simple")
 
-    def predict(self, text: str) -> dict[str, str]:
-        if entities := self.model(text):
-            return {
-                entity["word"]: entity["entity_group"]
+    def predict(self, text: str) -> dict[str, str] | list[ExtendedNerOutput]:
+        entities = self.model(text)
+        if not entities:
+            return {}
+
+        if Config.EXT_OUT:
+            out_list = []
+            out_list.extend(
+                {
+                    "value": entity.get("word", ""),
+                    "type": entity.get("entity_group", ""),
+                    "confidence": float(entity.get("score", 0.0)),
+                    "position": get_word_positions(text, entity.get("word", ""), entity.get("start", None)),
+                }
                 for entity in entities
                 if isinstance(entity, dict) and entity.get("score", 0) > Config.confidence_threshold and entity.get("word") is not None
-            }
+            )
+            return out_list
 
-        return {}
+        return {
+            entity["word"]: entity["entity_group"]
+            for entity in entities
+            if isinstance(entity, dict) and entity.get("score", 0) > Config.confidence_threshold and entity.get("word") is not None
+        }
