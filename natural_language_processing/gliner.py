32ceb2bc2f67f0072d396ec504b0afbb2c0a6189
llinauer <l.linauer@gmx.at>
2025-07-02 09:41:36 +0200

    Add extended output to gliner

--- natural_language_processing/gliner.py
+++ natural_language_processing/gliner.py
@@ -1,6 +1,7 @@
 from gliner import GLiNER
-from natural_language_processing.config import Config
+from natural_language_processing.config import Config, ExtendedNerOutput
 from natural_language_processing.predictor import Predictor
+from natural_language_processing.misc import get_word_positions
 
 
 class GLiNERModel(Predictor):
@@ -10,8 +11,27 @@ class GLiNERModel(Predictor):
         self.model = GLiNER.from_pretrained("selfconstruct3d/AITSecNER", load_tokenizer=True)
         self.labels = ["CLICommand/CodeSnippet", "CON", "DATE", "GROUP", "LOC", "MALWARE", "ORG", "SECTOR", "TACTIC", "TECHNIQUE", "TOOL"]
 
-    def predict(self, text: str) -> dict[str, str]:
-        if entities := self.model.predict_entities(text, self.labels, threshold=Config.confidence_threshold):
-            return {entity["text"]: entity["label"] for entity in entities if isinstance(entity, dict) and entity.get("score", 0)}
+    def predict(self, text: str) -> dict[str, str] | list[ExtendedNerOutput]:
+        entities = self.model.predict_entities(text, self.labels, threshold=Config.confidence_threshold)
+        if not entities:
+            return {}
 
-        return {}
+        if Config.EXT_OUT:
+            out_list = []
+            out_list.extend(
+                {
+                    "value": entity.get("text", ""),
+                    "type": entity.get("label", ""),
+                    "confidence": entity.get("score", 0.0),
+                    "position": get_word_positions(text, entity.get("text", ""), entity.get("start", None)),
+                }
+                for entity in entities
+                if isinstance(entity, dict) and entity.get("score", 0) > Config.confidence_threshold and entity.get("text") is not None
+            )
+            return out_list
+
+        return {
+            entity["text"]: entity.get("label", "")
+            for entity in entities
+            if isinstance(entity, dict) and entity.get("score", 0) > Config.confidence_threshold and entity.get("text") is not None
+        }
