f0e3d161cf744c006f05162c9d5f03e0a3c19d60
Peter Leitmann <peto.leitmann@gmail.com>
2025-09-05 16:07:31 +0200

    Use docker build and push action

--- build_container.sh
+++ build_container.sh
@@ -4,16 +4,18 @@ set -eou pipefail
 
 cd $(git rev-parse --show-toplevel)
 
-GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
+GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-"ghcr.io/taranis-ai"}
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
 MODEL=${MODEL:-"gliner"}
 
-echo "Building containers for branch ${CURRENT_BRANCH} on ${GITHUB_REPOSITORY_OWNER}"
+echo "Building containers for branch ${CURRENT_BRANCH} with the repository ${GITHUB_REPOSITORY} with model ${MODEL}"
+echo "$TAG_BOT"
+echo "$BOT_API_KEY"
 
 docker buildx build --file Containerfile \
-  --build-arg GITHUB_REPOSITORY_OWNER="${GITHUB_REPOSITORY_OWNER}" \
+  --build-arg GITHUB_REPOSITORY="${GITHUB_REPOSITORY}" \
   --build-arg MODEL="${MODEL}" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:latest" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:${CURRENT_BRANCH}" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-nlp-bot:${MODEL}" \
+  --tag "${GITHUB_REPOSITORY}/taranis-nlp-bot:latest" \
+  --tag "${GITHUB_REPOSITORY}/taranis-nlp-bot:${CURRENT_BRANCH}" \
+  --tag "${GITHUB_REPOSITORY}/taranis-nlp-bot:${MODEL}" \
   --load .
