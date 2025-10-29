1d8257ae5d40336860490a150df08fd1ec21de2c
llinauer <l.linauer@gmx.at>
2025-10-29 09:22:57 +0100

    Change PAYLOAD_KEY to "text"

--- natural_language_processing/config.py
+++ natural_language_processing/config.py
@@ -6,7 +6,7 @@ class Settings(CommonSettings):
     MODEL: Literal["gliner", "flair", "roberta", "roberta_german"] = "gliner"
     PACKAGE_NAME: str = "natural_language_processing"
     HF_MODEL_INFO: bool = True
-    PAYLOAD_KEY: str = "key"
+    PAYLOAD_KEY: str = "text"
     CONFIDENCE_THRESHOLD: float = 0.9
 
 
