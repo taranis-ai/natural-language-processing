c2feaef91885a65da1bf9b62198ca99f2530a7ef
llinauer <l.linauer@gmx.at>
2025-10-29 11:13:39 +0100

    Change PAYLOAD_KEY to now-used PAYLOAD_SCHEMA

--- natural_language_processing/config.py
+++ natural_language_processing/config.py
@@ -6,8 +6,12 @@ class Settings(CommonSettings):
     MODEL: Literal["gliner", "flair", "roberta", "roberta_german"] = "gliner"
     PACKAGE_NAME: str = "natural_language_processing"
     HF_MODEL_INFO: bool = True
-    PAYLOAD_KEY: str = "text"
-    CONFIDENCE_THRESHOLD: float = 0.9
+    PAYLOAD_SCHEMA: dict[str, dict] = {
+        "text": {"type": "str", "required": True},
+        "extended_output": {"type": "bool", "required": False},
+        "is_cybersecurity": {"type": "bool", "required": False},
+    }
+    CONFIDENCE_THRESHOLD: float = 0.7
 
 
 Config = Settings()
